dec9efe5a01c83f994175dbdc90e0c8514628a00
miguel <jzeaguerrero@gmail.com>
2017-05-26 12:20:35 -0500

    storage permissions

--- app/src/main/java/com/cerezaconsulting/compendio/presentation/activities/LoadActivity.java
+++ app/src/main/java/com/cerezaconsulting/compendio/presentation/activities/LoadActivity.java
@@ -1,21 +1,31 @@
 package com.cerezaconsulting.compendio.presentation.activities;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.cerezaconsulting.compendio.R;
 import com.cerezaconsulting.compendio.core.BaseActivity;
 import com.cerezaconsulting.compendio.data.local.SessionManager;
 
+import java.util.List;
 
-public class LoadActivity extends AppCompatActivity {
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+
+public class LoadActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+
+    private static final int RC_CAMERA_PERM = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState == null)
-            initialProcess();
+            methodRequiresPermissionCamera();
     }
 
 
@@ -38,4 +48,29 @@ public class LoadActivity extends AppCompatActivity {
         if (destroy) context.finish();
     }
 
+    @AfterPermissionGranted(RC_CAMERA_PERM)
+    private void methodRequiresPermissionCamera() {
+        String[] perms = {Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this.getApplicationContext(), perms)) {
+            initialProcess();
+        } else {
+            EasyPermissions.requestPermissions(this, getResources().getString(R.string.perm_camera),
+                    RC_CAMERA_PERM, perms);
+        }
+
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        if (requestCode == RC_CAMERA_PERM) {
+            initialProcess();
+        }
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+    }
 }
